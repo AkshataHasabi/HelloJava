6fb6645bf1fff8ab2ff0c44ab37d36b1393c730e
AkshataHasabi <akshuh818@gmail.com>
2021-12-22 17:55:57 +0530

    ADDED:welcome message

---
+++ HelloWorldUC1.java
@@ -0,0 +1,11 @@
+package javasamples;
+
+public class HelloWorldUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld Welcome To Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
