954d27ddd6b26797278e726c98de2aace63116ab
AkshataHasabi <akshuh818@gmail.com>
2021-12-22 17:54:26 +0530

    ADDED:initial commit

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+package javasamples;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld Welcome To Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
