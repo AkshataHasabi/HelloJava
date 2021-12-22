a668fcb370ec2eafd4f317341cc8e6fe35e94c5e
AkshataHasabi <akshuh818@gmail.com>
2021-12-22 17:57:13 +0530

    MODIFIED:used shortcutkeys:

---
+++ HelloWorldUC2.java
@@ -0,0 +1,11 @@
+package javasamples;
+
+public class HelloWorldUC2 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
